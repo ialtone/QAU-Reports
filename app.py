6b7c52121ddc09e571cd85b4eaea0b1f28841fb9
ialtone <1425592567@qq.com>
2022-09-26 11:15:01 +0800

    优化

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 from flask import Flask,render_template,request
 import json
-
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-
-@app.route('/',methods=['GET','POST'])
-def hello_world():  
-    if request.method == 'GET':
-        return render_template('index.html')
-    if request.method == 'POST':
-        Country = request.form.get('Country')
-        Province = request.form.get('Province')
-        City = request.form.get('City')
-        County = request.form.get('County')
-        street_number = request.form.get('street_number')
-        CurrentPosition = request.form.get('CurrentPosition')
-        FirstStitchDate = request.form.get('FirstStitchDate')
-        TwoStitchDate = request.form.get('TwoStitchDate')
-        ThreeStitchDate = request.form.get('ThreeStitchDate')
-        Vaccination = request.form.get('Vaccination')
-        BRHGTJZRSFYXLQKOne = request.form.get('BRHGTJZRSFYXLQKOne')
-        BRHGTJZRSFYXLQKTwo = request.form.get('BRHGTJZRSFYXLQKTwo')
-        SZSQTNSFFSYQ = request.form.get('SZSQTNSFFSYQ')
-        LJSHJCS = request.form.get('LJSHJCS')
-        BRHGTJZRSFYYXZZWQY = request.form.get('BRHGTJZRSFYYXZZWQY')
-        JRSFCSWFL = request.form.get('JRSFCSWFL')
-        JRSFCSNQTDJSFQ = request.form.get('JRSFCSNQTDJSFQ')
-        TodaySchedule = request.form.get('TodaySchedule')
-        DayTemperature = request.form.get('DayTemperature')
-        Remark = request.form.get('Remark')
-        TodayScheduleOther = request.form.get('TodayScheduleOther')
-        IsInSchoolOfDay = request.form.get('IsInSchoolOfDay')
-        ISNATDay = request.form.get('ISNATDay')
-        PushPlus = request.form.get('PushPlus')
-        username = request.form.get('username')
-        userpassword = request.form.get('userpassword')
-        PushPlustoken = request.form.get('PushPlustoken')
+def WirteIn():
+    Country = request.form.get('Country')
+    Province = request.form.get('Province')
+    City = request.form.get('City')
+    County = request.form.get('County')
+    street_number = request.form.get('street_number')
+    CurrentPosition = request.form.get('CurrentPosition')
+    FirstStitchDate = request.form.get('FirstStitchDate')
+    TwoStitchDate = request.form.get('TwoStitchDate')
+    ThreeStitchDate = request.form.get('ThreeStitchDate')
+    Vaccination = request.form.get('Vaccination')
+    BRHGTJZRSFYXLQKOne = request.form.get('BRHGTJZRSFYXLQKOne')
+    BRHGTJZRSFYXLQKTwo = request.form.get('BRHGTJZRSFYXLQKTwo')
+    SZSQTNSFFSYQ = request.form.get('SZSQTNSFFSYQ')
+    LJSHJCS = request.form.get('LJSHJCS')
+    BRHGTJZRSFYYXZZWQY = request.form.get('BRHGTJZRSFYYXZZWQY')
+    JRSFCSWFL = request.form.get('JRSFCSWFL')
+    JRSFCSNQTDJSFQ = request.form.get('JRSFCSNQTDJSFQ')
+    TodaySchedule = request.form.get('TodaySchedule')
+    DayTemperature = request.form.get('DayTemperature')
+    Remark = request.form.get('Remark')
+    TodayScheduleOther = request.form.get('TodayScheduleOther')
+    IsInSchoolOfDay = request.form.get('IsInSchoolOfDay')
+    ISNATDay = request.form.get('ISNATDay')
+    PushPlus = request.form.get('PushPlus')
+    username = request.form.get('username')
+    userpassword = request.form.get('userpassword')
+    PushPlustoken = request.form.get('PushPlustoken')
     ConfigData = {
         "cookies":{"insert_cookie":"29594869"},
         "NowDate":"",
@@ -79,6 +71,15 @@ def hello_world():
         json.dump(ConfigData,f1,ensure_ascii=False,indent=4)
     with open(user,'w',encoding='utf-8') as f2:
         json.dump(UserData,f2,ensure_ascii=False,indent=4)
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/',methods=['GET','POST'])
+def main():  
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        WirteIn()
     with open('config.json','r',encoding='utf-8') as f3:
         line1 = f3.read()
     with open('user.json','r',encoding='utf-8') as f4:
